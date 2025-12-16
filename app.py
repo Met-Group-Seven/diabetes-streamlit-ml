758cabe851603ef9c526d96359cbedbe4f7086f2
Met-Group-Seven <ketelephone@gmail.com>
2025-12-16 19:03:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ scaler = joblib.load("group7_scaler.joblib")
 # ==========================
 # Segment 3: App Title
 # ==========================
-st.title("GROUP 7 Diabetes Prediction System")
+st.title("Group 7: Diabetes Prediction System")
 
 # ==========================
 # Segment 4: User Input
